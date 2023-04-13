7e890faec5768b36c8fa3c8461cef8757e2f6c9b
Hugo Caillard <911307+hugocaillard@users.noreply.github.com>
2023-04-13 19:03:00 +0200

    chore: fix site title

--- app/(explorer)/head.tsx
+++ app/(explorer)/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Ordinal Explorer</title>
+      <title>Ordinals Explorer</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Exploring ordinals" />
       <link rel="icon" href="/favicon.ico" />
