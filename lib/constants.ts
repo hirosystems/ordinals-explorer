9c64ca6373b2b979ffb1d67172b3e1badaec2f8f
janniks <janniks@users.noreply.github.com>
2023-04-24 16:28:11 +0200

    chore: typos

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const TILE_SIZE = 16;
 
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL ?? "http://api.hiro.so/ordinals/v1";
+  process.env.NEXT_PUBLIC_API_URL ?? "https://api.hiro.so/ordinals/v1";
