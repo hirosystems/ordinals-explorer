bc0e5adb20860345a31914e5d9cd8aa65a666846
janniks <janniks@users.noreply.github.com>
2023-06-05 16:19:05 +0200

    refactor: disable header navs

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,33 +7,41 @@ import { HoverCard, HoverCardContent, HoverCardTrigger } from "./HoverCard";
 
 const Header = ({ children }: { children?: React.ReactNode }) => {
   return (
-    <header className="relative flex justify-between px-6 md:px-10 py-8 w-full max-w-[88rem] mx-auto">
+    <header className="relative mx-auto flex w-full max-w-[88rem] justify-between px-6 py-8 md:px-10">
       <a href="/">
         <img src="/logo.svg" alt="Hiro Ordinals Beta" />
       </a>
       {children}
       {/* todo: explore button, stats, hiro.so */}
       <div className="hidden sm:block">
-        <HoverCard openDelay={0}>
-          <HoverCardTrigger className="px-3.5 py-2.5 rounded-md cursor-default hover:bg-neutral-0 select-none">
+        <Link
+          href="/explore"
+          className="rounded-md px-3.5 py-2.5 hover:bg-neutral-0"
+        >
+          Explore All
+        </Link>
+        {/* todo: re-enable different explore modes */}
+        {/* <HoverCard openDelay={0}>
+          <HoverCardTrigger className="cursor-default select-none rounded-md px-3.5 py-2.5 hover:bg-neutral-0">
             Explore
           </HoverCardTrigger>
           <HoverCardContent className="flex flex-col p-1">
             <Link
               href="/explore"
-              className="px-3.5 py-2.5 rounded-md hover:bg-neutral-0"
+              className="rounded-md px-3.5 py-2.5 hover:bg-neutral-0"
             >
               All inscriptions
             </Link>
             <Link
               href="/period"
-              className="px-3.5 py-2.5 rounded-md hover:bg-neutral-0"
+              className="rounded-md px-3.5 py-2.5 hover:bg-neutral-0"
             >
               By halving period
             </Link>
           </HoverCardContent>
-        </HoverCard>
-        <HoverCard openDelay={0}>
+        </HoverCard> */}
+        {/* todo: re-enable stats nav */}
+        {/* <HoverCard openDelay={0}>
           <HoverCardTrigger className="opacity-50 cursor-not-allowed">
             <span
               // todo: change back to Link
@@ -46,11 +54,11 @@ const Header = ({ children }: { children?: React.ReactNode }) => {
           <HoverCardContent className="px-2">
             Under Construction 🚧
           </HoverCardContent>
-        </HoverCard>
+        </HoverCard> */}
         <Link
           target="_blank"
           href="https://hiro.so"
-          className="px-3.5 py-2.5 rounded-md text-neutral-300 hover:text-neutral-500"
+          className="rounded-md px-3.5 py-2.5 text-neutral-300 hover:text-neutral-500"
         >
           hiro.so
           <ArrowUpRight className="inline" size={16} />
@@ -58,7 +66,7 @@ const Header = ({ children }: { children?: React.ReactNode }) => {
       </div>
       {process.env.NODE_ENV !== "production" && (
         // breakpoint debugging during development
-        <div className="absolute p-2 top-0 right-0 text-xs text-neutral-200">
+        <div className="absolute right-0 top-0 p-2 text-xs text-neutral-200">
           <span className="inline sm:hidden">xs</span>
           <span className="hidden sm:inline md:hidden">sm</span>
           <span className="hidden md:inline lg:hidden">md</span>
