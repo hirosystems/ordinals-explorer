74eabe18e726c43c1132da9d0dce8e2c539251a5
janniks <janniks@users.noreply.github.com>
2023-06-12 20:57:08 +0200

    fix: allow pointer events on iframe

--- components/inscriptions/Iframe.tsx
+++ components/inscriptions/Iframe.tsx
@@ -4,10 +4,7 @@ const Iframe = (props: { src: string; className?: string }) => {
   return (
     <iframe
       src={props.src}
-      className={cn(
-        "pointer-events-none aspect-square h-full w-full border-none",
-        props.className
-      )}
+      className={cn("aspect-square h-full w-full border-none", props.className)}
       loading="lazy"
       sandbox="allow-scripts"
       scrolling="no"
