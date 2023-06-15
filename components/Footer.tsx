026d6bf84c5ae00d7896d3cba317c66e1e8e04bd
janniks <janniks@users.noreply.github.com>
2023-06-15 15:28:49 +0200

    feat: add fullscreen viewer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,23 +1,21 @@
 import { GitHubLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
+import IconTwitter from "./icons/IconTwitter";
 
 const Footer = () => {
   return (
-    <footer className="p-3 sm:p-5 md:p-10 w-full max-w-[88rem] mx-auto">
-      <div className="w-full bg-black rounded-md">
-        <div className="p-4 sm:p-6 md:p-12 pb-8 flex flex-col">
+    <footer className="mx-auto w-full max-w-[88rem] p-3 sm:p-5 md:p-10">
+      <div className="w-full rounded-md bg-black">
+        <div className="flex flex-col p-4 pb-8 sm:p-6 md:p-12">
           <a href="https://hiro.so" aria-label="Hiro Systems">
-            <div className="hiro w-[54px] h-[54px]"></div>
+            <div className="hiro h-[54px] w-[54px]"></div>
           </a>
           {/* todo: link chainhooks */}
-          <p className="my-4 md:my-6 text-sm text-neutral-0">
+          <p className="my-4 text-sm text-neutral-0 md:my-6">
             This Ordinals Explorer is using the{" "}
-            <a
-              className="underline"
-              href="https://docs.hiro.so/ordinals"
-            >
+            <a className="underline" href="https://docs.hiro.so/ordinals">
               Hiro Ordinals API
             </a>{" "}
-              powered by{" "}
+            powered by{" "}
             <a
               className="underline"
               href="https://github.com/hirosystems/chainhook"
@@ -26,20 +24,20 @@ const Footer = () => {
             </a>
             .
           </p>
-          <div className="mt-10 flex flex-col md:flex-row justify-between text-center text-xs text-neutral-300 space-y-4 md:space-y-0">
+          <div className="mt-10 flex flex-col justify-between space-y-4 text-center text-xs text-neutral-300 md:flex-row md:space-y-0">
             <div>
               {/* todo: links */}
               <a href="https://www.hiro.so/patent-pledge">Patent Pledge</a>{" "}
               &mdash;{" "}
               <a href="https://www.hiro.so/terms-privacy">Terms & Privacy</a>
             </div>
-            <div className="space-x-3">
+            <div className="flex justify-center space-x-3">
               <a href="https://twitter.com/hirosystems">
                 {/* todo: find filled twitter icon */}
-                <TwitterLogoIcon className="inline-block w-5 h-5 text-neutral-0" />
+                <IconTwitter className="inline-block h-3.5 text-neutral-0" />
               </a>
               <a href="https://github.com/hirosystems">
-                <GitHubLogoIcon className="inline-block w-[18px] h-[18px] text-neutral-0" />
+                <GitHubLogoIcon className="inline-block h-[18px] w-[18px] text-neutral-0" />
               </a>
               <a href="https://hiro.so" className="inline-block">
                 &copy; {new Date().getFullYear()} Hiro Systems PBC
