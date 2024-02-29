d9a6f97de6f5a1c22f06476e92a318a3ca3bafd3
janniks <6362150+janniks@users.noreply.github.com>
2024-02-29 18:51:08 +0100

    fix: update digioh form style (#107)
    Co-authored-by: janniks <janniks@users.noreply.github.com>

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,9 @@
 "use client";
 
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import { motion } from "framer-motion";
-import { GitHubLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
-import IconTwitter from "./icons/IconTwitter";
 import Link from "next/link";
+import IconTwitter from "./icons/IconTwitter";
 
 const Footer = () => {
   return (
@@ -11,17 +11,17 @@ const Footer = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
-      className="mx-auto w-full max-w-[88rem] p-3 sm:p-5 md:p-10"
+      className="mx-auto w-full max-w-[88rem] p-3 sm:p-5 md:p-6 lg:p-10"
     >
       <div className="w-full rounded-md bg-black">
-        <div className="flex flex-col p-4 pb-8 sm:p-6 md:p-12">
-          <div className="flex justify-between gap-4">
-            <div className="flex flex-col">
+        <div className="flex flex-col p-4 pb-8 sm:p-6 md:p-8 lg:p-12">
+          <div className="flex flex-col justify-between gap-6 md:flex-row">
+            <div className="flex gap-4 md:flex-col">
               <a href="https://hiro.so" aria-label="Hiro Systems">
-                <div className="hiro h-[54px] w-[54px]"></div>
+                <div className="hiro h-[54px] w-[54px]" />
               </a>
               {/* todo: link chainhooks */}
-              <p className="my-4 text-sm text-neutral-0 md:my-6">
+              <p className="text-sm text-neutral-0">
                 This Ordinals Explorer is using the{" "}
                 <a className="underline" href="https://docs.hiro.so/ordinals">
                   Hiro Ordinals API
@@ -37,9 +37,10 @@ const Footer = () => {
               </p>
             </div>
             {/* Newsletter form */}
-            <div className="flex basis-[420px]">
-              <div id="lightbox-inline-form-8b0bbe84-51ac-4bfc-8af0-101231368a57" />
-            </div>
+            <div
+              className="digioh-form"
+              id="lightbox-inline-form-8b0bbe84-51ac-4bfc-8af0-101231368a57"
+            />
           </div>
           <div className="mt-10 flex flex-col justify-between space-y-4 text-center text-xs text-neutral-300 md:flex-row md:space-y-0">
             <div>
